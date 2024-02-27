e6fc5c618274e81485f42387ba1f1c177d98d7c7
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2024-02-27 19:38:51 +0100

    toggle

--- script/script-lesson-4.js
+++ script/script-lesson-4.js
@@ -1,5 +1,18 @@
-$(document).ready(() => {
-    $("lesson-4").on('load', () => {
 
+$(document).ready(function() {
+    $("#lesson-4").on('load', function() {
+
+        $(this).contents().find(".show").click(() => {
+            $(this).contents().find(".content").show(1500);
+        });
+
+        $(this).contents().find(".hide").click(() => {
+            $(this).contents().find(".content").hide(1500);
+        });
+
+        $(this).contents().find(".toggle").click(() => {
+            $(this).contents().find(".content").toggle(1000);
+            $(this).contents().find(".title").toggle(500);
+        });
     });
 });
