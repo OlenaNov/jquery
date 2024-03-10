7913c7c8f16e9c73a99b040f932d98db34eee4c3
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2024-03-10 18:00:16 +0100

    refactoring

--- script/body.js
+++ script/body.js
@@ -1,60 +1,14 @@
 
-// $(document).ready(function() {
-//     function handleLessonsList(lessonNumber) {
-//             $(this).contents().find(`#lesson-${lessonNumber}`).slideToggle(1000);
-//         };
-
-//     $(this).contents().find(".lesson").click(function() {
-//        console.log("lessonNumber");
-//         let lessonNumber = $(this).data("lesson");
-//        handleLessonsList(lessonNumber);
-//     });
-// });
-
-
 $(document).ready(function() {
-        $(this).contents().find(".lesson-2").click(() => {
-            $(this).contents().find("#lesson-2").slideToggle(500);
-        });
 
-        $(this).contents().find(".lesson-3").click(() => {
-            $(this).contents().find("#lesson-3").slideToggle(1000);
-        });
+    function handleLessonsList(lessonNumber) {
+        console.log(`#lesson-${lessonNumber}`);
+        $(`#lesson-${lessonNumber}`).slideToggle(1000);
 
-        $(this).contents().find(".lesson-4").click(() => {
-            $(this).contents().find("#lesson-4").slideToggle(1000);
-        });
+        };
 
-        $(this).contents().find(".lesson-5").click(() => {
-            $(this).contents().find("#lesson-5").slideToggle(1000);
-        });
-
-
-        $(this).contents().find(".lesson-6").click(() => {
-            $(this).contents().find("#lesson-6").slideToggle(1000);
-        });
-
-        $(this).contents().find(".lesson-7").click(() => {
-            $(this).contents().find("#lesson-7").slideToggle(1000);
-        });
-
-        $(this).contents().find(".lesson-8").click(() => {
-            $(this).contents().find("#lesson-8").slideToggle(1000);
-        });
-
-        $(this).contents().find(".lesson-9").click(() => {
-            $(this).contents().find("#lesson-9").slideToggle(1000);
-        });
-
-        $(this).contents().find(".lesson-10").click(() => {
-            $(this).contents().find("#lesson-10").slideToggle(1000);
-        });
-
-        $(this).contents().find(".lesson-11").click(() => {
-            $(this).contents().find("#lesson-11").slideToggle(1000);
-        });
-
-        $(this).contents().find(".lesson-12").click(() => {
-            $(this).contents().find("#lesson-12").slideToggle(1000);
-        });
+    $(".lesson").click(function() {
+        let lessonNumber = $(this).data("lesson");
+       handleLessonsList(lessonNumber);
+    });
 });
